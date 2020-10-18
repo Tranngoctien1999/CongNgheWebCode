d273ad0514b4a544e41c665433f9b8c9b75ad868
phamxuantien <phamxuantien.1117@gmail.com>
2020-10-18 21:43:03 +0700

    Revert "Xuân tiến add vào thành công"
    This reverts commit a866361eaa326c3b97eb595e3dbfdfcfb161133f.

--- BanVeDiTourDuLich/Controllers/UserController.cs
+++ BanVeDiTourDuLich/Controllers/UserController.cs
@@ -8,42 +8,10 @@ namespace BanVeDiTourDuLich.Controllers
 {
     public class UserController : Controller
     {
-        DataContext _db = new DataContext();
         // GET: User
-      
         public ActionResult Index()
         {
             return View();
         }
-
-
-
-        [HttpPost]
-        public ActionResult Index(string TaiKhoanDangNhap, string password)
-        {
-            if (ModelState.IsValid)
-            {
-
-
-              
-                var data = _db.TaiKhoans.Where(s => s.TaiKhoanDangNhap.Equals(TaiKhoanDangNhap) && s.MatKhau.Equals(password)).ToList();
-                if (data.Count() > 0)
-                {
-                    //add session
-                   
-                    Session["MaTaiKhoan"] = data.FirstOrDefault().MaTaiKhoan;
-                    Session["TaiKhoanDangNhap"] = data.FirstOrDefault().TaiKhoanDangNhap;
-                    return RedirectToAction("/home/index");
-                }
-                else
-                {
-                    ViewBag.error = "Login failed";
-                    return RedirectToAction("Index");
-                }
-            }
-            return View();
-        }
-
-
     }
 }
