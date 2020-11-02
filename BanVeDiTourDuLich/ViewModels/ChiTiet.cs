bbc7918bdeca4372c62e4d517e04106315fb9d02
ngoctien <ngoctien18051999@gmail.com>
2020-11-02 13:57:48 +0700

    update form search

---
+++ BanVeDiTourDuLich/ViewModels/ChiTiet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanVeDiTourDuLich.ViewModels
+{
+    public class ChiTiet
+    {
+        public string DuongDanAnh { get; set; }
+        public string TenDiaDiem { get; set; }
+        public double GiaTien { get; set; }
+        public Tour Tour { get; set; }
+    }
+}
