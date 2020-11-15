cebf479ef3c403de6511f82a67b4988a4c2a5d80
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-11-15 22:46:35 +0700

    Add A dummy migration

---
+++ BanVeDiTourDuLich/Migrations/202011151545194_pending.cs
@@ -0,0 +1,18 @@
+﻿namespace BanVeDiTourDuLich.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class pending : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
