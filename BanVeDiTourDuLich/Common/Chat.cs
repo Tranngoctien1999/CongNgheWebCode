13c6e37e8a3212f1583760d5b12667e7a66b9bd4
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-11-18 21:34:01 +0700

    isfixing chat sync why the hub run the functions twice

--- BanVeDiTourDuLich/Common/Chat.cs
+++ BanVeDiTourDuLich/Common/Chat.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Data.Entity.Migrations;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using BanVeDiTourDuLich.Models;
 using BanVeDiTourDuLich.Utilizer;
@@ -23,11 +24,13 @@ namespace BanVeDiTourDuLich.Hubs
             DataContext = new DataContext();
         }
 
-        // Lưu lại tin nhắn gửi từ khách hàng tới nhân viên cụ thể
-        public void SaveNewMessageFromKhachHangToNhanVien(string maNhanVien , string maKhachHang, string noiDung)
+        // giữa nhân viên và khách hàng
+
+        // Lưu lại tin nhắn gửi giữa khách hàng và nhân viên cụ thể
+        public async Task SaveNewMessageBetweenNhanVienAndClient(string maNhanVien , string maKhachHang, string noiDung)
         {
-            KhachHang khachHang = DataContext.KhachHangs.Find(maKhachHang);
-            NhanVien nhanVien = DataContext.NhanViens.Find(maNhanVien);
+            KhachHang khachHang = await DataContext.KhachHangs.FindAsync(maKhachHang);
+            NhanVien nhanVien = await DataContext.NhanViens.FindAsync(maNhanVien);
             TinNhan tinNhan = new TinNhan()
             {
                 MaKhachHang = khachHang.MaKhachHang,
@@ -36,13 +39,17 @@ namespace BanVeDiTourDuLich.Hubs
                 ThoiGianGui = DateTime.Now
             };
             DataContext.TinNhans.Add(tinNhan);
-            DataContext.SaveChanges();
+            await DataContext.SaveChangesAsync();
         }
 
+
+
+        // Khách hàng gửi tin
+
         // Lưu lại tin nhắn khách hàng gửi mà chưa có nhân viên trợ giúp
-        public void SaveNewMessageFromKhachHang(string maKhachHang, string noiDung)
+        public async Task SaveNewMessageFromKhachHang(string maKhachHang, string noiDung)
         {
-            KhachHang khachHang = DataContext.KhachHangs.Find(maKhachHang);
+            KhachHang khachHang = await DataContext.KhachHangs.FindAsync(maKhachHang);
             TinNhan tinNhan = new TinNhan()
             {
                 MaKhachHang = khachHang.MaKhachHang,
@@ -50,12 +57,13 @@ namespace BanVeDiTourDuLich.Hubs
                 ThoiGianGui = DateTime.Now
             };
             DataContext.TinNhans.Add(tinNhan);
-            DataContext.SaveChanges();
+            await DataContext.SaveChangesAsync();
         }
         //Gửi tin nhắn từ khách hàng tới đối tượng cụ thể
-        public void SendMessageFromClientToManager(string maNhanVien,string maKhachHang)
+        public async Task SendMessageFromClientToManager(string maNhanVien,string maKhachHang)
         {
-            TinNhan tinNhanCuoi = DataContext.TinNhans.Last(m => m.MaKhachHang == maKhachHang);
+            TinNhan tinNhanCuoi = await DataContext.TinNhans.Where(m => m.MaKhachHang == maKhachHang)
+                .OrderByDescending(c => c.ThoiGianGui).FirstAsync();
             ConnectionIdUser connection = connections.Find(c => c.MaTaiKhoan == maNhanVien);
             if (connection != null && tinNhanCuoi != null)
             {
@@ -64,35 +72,64 @@ namespace BanVeDiTourDuLich.Hubs
         }
 
         // Gửi tin nhắn tới tất cả các nhân viên ( hàm này cần chỉnh sao cho phù hợp với admin page)
-        public void SendMassageFromClientToAllManager(string maKhachHang, string noiDung)
+        public async Task SendMassageFromClientToAllManager(string maKhachHang, string noiDung)
         {
-            TinNhan tinNhanCuoi = DataContext.TinNhans.Where(m => m.MaKhachHang == maKhachHang)
-                .OrderByDescending(c => c.ThoiGianGui).First();
+            TinNhan tinNhanCuoi = await DataContext.TinNhans.Where(m => m.MaKhachHang == maKhachHang)
+                .OrderByDescending(c => c.ThoiGianGui).FirstAsync();
             if (tinNhanCuoi != null)
             {
                 Clients.Clients(manager.Select(m => m.ConnectionId).ToList()).addNewMessageToManager(maKhachHang,noiDung,tinNhanCuoi.ThoiGianGui.ToString("d dddd-M-yyyy"));
+                Clients.Clients(manager.Select(m => m.ConnectionId).ToList()).addMessageInformationInCard(maKhachHang,noiDung,tinNhanCuoi.ThoiGianGui.ToString("d dddd-M-yyyy"));
             }
         }
 
-        public void SendMassageFromDbToCurrentBrower(string maKhachHang)
+        public async Task SendMassageFromDbToCurrentClientBrower(string maTaiKhoan)
         {
-            TinNhan tinNhanCuoi = DataContext.TinNhans.Where(m => m.MaKhachHang == maKhachHang)
-                .OrderByDescending(c => c.ThoiGianGui).First();
-            ConnectionIdUser connection = connections.Find(c => c.MaTaiKhoan == maKhachHang);
+            TinNhan tinNhanCuoi = await DataContext.TinNhans.Where(m => m.MaKhachHang == maTaiKhoan)
+                .OrderByDescending(c => c.ThoiGianGui).FirstAsync();
+            ConnectionIdUser connection = connections.Find(c => c.MaTaiKhoan == maTaiKhoan);
             if (connection != null && tinNhanCuoi != null)
             {
-                Clients.Client(connection.ConnectionId).addNewMessageCurrentClientBrower(tinNhanCuoi.NoiDung , tinNhanCuoi.ThoiGianGui.ToString("d dddd-M-yyyy"));
+                Clients.Client(connection.ConnectionId).sendMassageFromDbToCurrentClientBrower(tinNhanCuoi.NoiDung , tinNhanCuoi.ThoiGianGui.ToString("d dddd-M-yyyy"));
             }
         }
 
-        // Thêm connection Id khi người dùng hoặc nhân viên đăng nhập vào
-        public void AddConnectionId(string maTaiKhoan, string connectionId)
+        // Nhân viên gửi tin
+
+        // Gửi tin nhắn cho khách hàng
+        public async Task SendMassageFromManagerToClient(string maNhanVien, string maKhachHang, string noiDung)
         {
-            TaiKhoan taiKhoan = DataContext.TaiKhoans.Find(maTaiKhoan);
+            TinNhan tinNhanCuoi = await DataContext.TinNhans.Where(m => m.MaNhanVien == maNhanVien)
+                .OrderByDescending(c => c.ThoiGianGui).FirstAsync();
+            ConnectionIdUser connection = user.Find(c => c.MaTaiKhoan == maKhachHang);
+            if (connection != null && tinNhanCuoi != null)
+            {
+                Clients.Client(connection.ConnectionId).addNewMessageToClient(maNhanVien,tinNhanCuoi.NoiDung , tinNhanCuoi.ThoiGianGui.ToString("d dddd-M-yyyy"));
+            }
+        }
+        
+        // Gửi tin nhắn cho người nhân viên hiện tại
+        public async Task SendMassageFromDbToCurrentManagerBrower(string maTaiKhoan)
+        {
+            TinNhan tinNhanCuoi = await DataContext.TinNhans.Where(m => m.MaNhanVien == maTaiKhoan)
+                .OrderByDescending(c => c.ThoiGianGui).FirstAsync();
+            ConnectionIdUser connection = connections.Find(c => c.MaTaiKhoan == maTaiKhoan);
+            if (connection != null && tinNhanCuoi != null)
+            {
+                Clients.Client(connection.ConnectionId).addNewMessageCurrentManagerBrower(tinNhanCuoi.NoiDung , tinNhanCuoi.ThoiGianGui.ToString("d dddd-M-yyyy"));
+            }
+        }
+
+        // 
+
+        // Thêm connection Id khi người dùng hoặc nhân viên đăng nhập vào
+        public async Task AddConnectionId(string maTaiKhoan, string connectionId)
+        {
+            TaiKhoan taiKhoan = await DataContext.TaiKhoans.FindAsync(maTaiKhoan);
             if (taiKhoan != null)
             {
                 taiKhoan.ConnectionId = connectionId;
-                DataContext.SaveChanges();
+                await DataContext.SaveChangesAsync();
                 if (taiKhoan.KhachHang != null)
                 {
                     user.Add(new ConnectionIdUser(){MaTaiKhoan = taiKhoan.MaTaiKhoan , ConnectionId = taiKhoan.ConnectionId});
@@ -106,12 +143,12 @@ namespace BanVeDiTourDuLich.Hubs
         }
 
         // Xóa connection Id khi người dùng ngắt kết nối
-        public void DeleteConnectionId(string maTaiKhoan, string connectionId)
+        public async Task DeleteConnectionId(string maTaiKhoan, string connectionId)
         {
-            TaiKhoan taiKhoan = DataContext.TaiKhoans.Find(maTaiKhoan);
+            TaiKhoan taiKhoan = await DataContext.TaiKhoans.FindAsync(maTaiKhoan);
             if (taiKhoan != null)
             {
-                DataContext.SaveChanges();
+                await DataContext.SaveChangesAsync();
                 ConnectionIdUser currentConnection = connections.First(connection => connection.MaTaiKhoan == taiKhoan.MaTaiKhoan);
                 connections.Remove(currentConnection);
             }
