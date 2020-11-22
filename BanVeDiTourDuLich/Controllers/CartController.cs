ea1e83f6078c6d45d35eec721ebce05074b79682
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-11-22 21:57:52 +0700

    Adding Stripe

--- BanVeDiTourDuLich/Controllers/CartController.cs
+++ BanVeDiTourDuLich/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,10 @@ namespace BanVeDiTourDuLich.Controllers
         // GET: Cart
         public ActionResult Index()
         {
+            StripeKey key = new StripeKey(){
+                PublishableKey = ConfigurationManager.AppSettings["stripePublishableKey"],
+                SecretKey = ConfigurationManager.AppSettings["stripeSecretKey"]
+            };
             if (Session["MaTaiKhoan"] != null)
             {
                 List<ThongTinVeTrongGio> gioHang = new List<ThongTinVeTrongGio>();
