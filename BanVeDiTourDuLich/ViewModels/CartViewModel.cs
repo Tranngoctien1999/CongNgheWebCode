cbf31d6e8c8f19dff24982798172ea1188346c17
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-11-23 07:02:03 +0700

    Update Checkout

---
+++ BanVeDiTourDuLich/ViewModels/CartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanVeDiTourDuLich.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<ThongTinVeTrongGio> ThongTinVeTrongGios { get; set; }
+        public StripeKey StripeKey { get; set; }
+    }
+}
