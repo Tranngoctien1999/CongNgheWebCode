ec66a8547200475c13ab90e3c749cae8e7bc2c67
ngoctien <ngoctien18051999@gmail.com>
2020-11-27 08:15:27 +0700

    update quan tri

---
+++ BanVeDiTourDuLich/ViewModels/ChiTietTourAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanVeDiTourDuLich.ViewModels
+{
+    public class ChiTietTourAdmin
+    {
+        public Tour Tour { get; set; }
+        public LoaiVe LoaiVe { get; set; }
+    }
+}
